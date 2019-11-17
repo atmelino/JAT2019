6c2f7344fdab9062a4e1bb90898db39f44960c1b
tmeng12 <tmbscience@gmail.com>
2019-11-16 20:52:57 -0600

    test

---
+++ lwjglbook/src/chapter02/game/Main.java
@@ -0,0 +1,19 @@
+package chapter02.game;
+
+import chapter02.engine.GameEngine;
+import chapter02.engine.IGameLogic;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.run();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
