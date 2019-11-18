e7198265996bb7c190265ee7f0cdfe63441a60b2
tmeng12 <tmbscience@gmail.com>
2019-11-17 23:26:40 -0600

    rectangle

---
+++ lwjglbook/src/chapter06p1/game/Renderer.java
@@ -0,0 +1,78 @@
+package chapter06p1.game;
+
+import org.joml.Matrix4f;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
+import chapter06p1.engine.Utils;
+import chapter06p1.engine.Window;
+import chapter06p1.engine.graph.Mesh;
+import chapter06p1.engine.graph.ShaderProgram;
+
+public class Renderer {
+
+    /**
+     * Field of View in Radians
+     */
+    private static final float FOV = (float) Math.toRadians(60.0f);
+
+    private static final float Z_NEAR = 0.01f;
+
+    private static final float Z_FAR = 1000.f;
+
+    private Matrix4f projectionMatrix;
+
+    private ShaderProgram shaderProgram;
+
+    public Renderer() {
+    }
+
+    public void init(Window window) throws Exception {
+        // Create shader
+        shaderProgram = new ShaderProgram();
+        shaderProgram.createVertexShader(Utils.loadResource("../resources/vertex.vs"));
+        shaderProgram.createFragmentShader(Utils.loadResource("../resources/fragment.fs"));
+        shaderProgram.link();
+        
+        // Create projection matrix
+        float aspectRatio = (float) window.getWidth() / window.getHeight();
+        projectionMatrix = new Matrix4f().perspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+        shaderProgram.createUniform("projectionMatrix");
+    }
+
+    public void clear() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
+
+    public void render(Window window, Mesh mesh) {
+        clear();
+
+        if ( window.isResized() ) {
+            glViewport(0, 0, window.getWidth(), window.getHeight());
+            window.setResized(false);
+        }
+
+        shaderProgram.bind();
+        shaderProgram.setUniform("projectionMatrix", projectionMatrix);
+
+        // Draw the mesh
+        glBindVertexArray(mesh.getVaoId());
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, mesh.getVertexCount(), GL_UNSIGNED_INT, 0);
+
+        // Restore state
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+
+        shaderProgram.unbind();
+    }
+
+    public void cleanup() {
+        if (shaderProgram != null) {
+            shaderProgram.cleanup();
+        }
+    }
+}
---
+++ lwjglbook/src/chapter06p2/engine/IGameLogic.java
@@ -0,0 +1,14 @@
+package chapter06p2.engine;
+
+public interface IGameLogic {
+
+    void init(Window window) throws Exception;
+    
+    void input(Window window);
+
+    void update(float interval);
+    
+    void render(Window window);
+    
+    void cleanup();
+}
